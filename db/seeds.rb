c68b31a5bfa6a31f10fd9b2e69f48be86eed166f
Nguyễn Thịnh <nvthinh.hut.hedspi@gmail.com>
2015-09-12 14:22:50 +0000

    Basic add new rel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,21 @@
-User.create!(user_name: "bachdx",
-              encrypted_password: "$2a$10$fLBwpa/3Q4NTQWZTJhOIZ.MXp6xNAWoszt76iHlfyA.woUEpaM3QG",
-              role: "1")
+admins = [
+  { user_name: "bachdx",
+    encrypted_password: "$2a$10$fLBwpa/3Q4NTQWZTJhOIZ.MXp6xNAWoszt76iHlfyA.woUEpaM3QG",
+    role: "1" },
+  { user_name: "thinhnv",
+    encrypted_password: "$2a$10$fLBwpa/3Q4NTQWZTJhOIZ.MXp6xNAWoszt76iHlfyA.woUEpaM3QG",
+    role: "1" }
+]
+
+admins.each do |admin|
+  User.create! admin
+end
 
 heroes= [
  { name: "Abaddon", hero_type: Hero::STRENGTH, description: "abaddon created by thinh ngu vcl", img_url: "abaddon.png"}
 ]
 
 heroes.each do |hero|
-  Rails.logger.debug hero
   Hero.create! hero
 end
 
@@ -26,6 +34,6 @@ divides = [
   { name: "Golden" }
 ]
 
-divides.each do |devide|
+divides.each do |divide|
   Divide.create! divide
 end
